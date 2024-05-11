3fe64fe19983b75379f9027e1c95f71474475fe8
nikhil1905-n <nikhil1905.n@gmail.com>
2024-05-12 02:36:02 +0530

    Add new badge types

--- src/main/java/com/nikhiln/coderHack/model/Badges.java
+++ src/main/java/com/nikhiln/coderHack/model/Badges.java
@@ -2,4 +2,23 @@ package com.nikhiln.coderHack.model;
 
 public enum Badges {
 
+    // Badge awarded for beginners who have shown promise
+    Code_Ninja("Code Ninja"),
+
+    // Badge awarded for coders who have mastered the basics
+    Code_Champ("Code Champ"),
+
+    // Badge awarded for coders who have demonstrated advanced skills
+    Code_Master("Code Master");
+
+    private final String badgeName;
+
+    Badges(String badgeName) {
+        this.badgeName = badgeName;
+    }
+
+    public String getBadgeName() {
+        return this.badgeName;
+    }
+
 }
