197a656044555bd749b1972166bae02e2cbe9c3d
nikhil1905-n <nikhil1905.n@gmail.com>
2024-05-13 15:13:29 +0530

    Implement repository layer

--- src/main/java/com/nikhiln/coderHack/repository/CoderHackRepository.java
+++ src/main/java/com/nikhiln/coderHack/repository/CoderHackRepository.java
@@ -2,7 +2,9 @@ package com.nikhiln.coderHack.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.nikhiln.coderHack.model.User;
-
+import java.util.List;
 public interface CoderHackRepository extends MongoRepository<User, String> {
+
+    List<User> findAllByOrderByScoreDesc();
     
 }
