15b7d62c7acebbb25a186018d450d9c2c342218d
nikhil1905-n <nikhil1905.n@gmail.com>
2024-05-13 15:11:00 +0530

    Implement exception layer

--- src/main/java/com/nikhiln/coderHack/exception/UserAlreadyExistsException.java
+++ src/main/java/com/nikhiln/coderHack/exception/UserAlreadyExistsException.java
@@ -1,5 +1,15 @@
 package com.nikhiln.coderHack.exception;
 
-public class UserAlreadyExistsException {
+public class UserAlreadyExistsException extends RuntimeException {
+
+    private static final String DEFAULT_MSG = "User Already Exists";
+
+    public UserAlreadyExistsException() {
+        super(DEFAULT_MSG);
+    }
+
+    public UserAlreadyExistsException(String msg) {
+        super(msg);
+    }
     
 }
--- src/main/java/com/nikhiln/coderHack/exception/UserNotFoundException.java
+++ src/main/java/com/nikhiln/coderHack/exception/UserNotFoundException.java
@@ -1,5 +1,15 @@
 package com.nikhiln.coderHack.exception;
 
-public class UserNotFoundException {
+public class UserNotFoundException extends RuntimeException {
+
+    private static final String DEFAULT_MSG = "User Not Found";
+
+    public UserNotFoundException() {
+        super(DEFAULT_MSG);
+    }
+
+    public UserNotFoundException(String msg) {
+        super(msg);
+    }
     
 }
